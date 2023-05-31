6d446d26279b2b6da6fd1aa55988000305ad2bbe
Prakashini Murugan <prakashini.murugan@outlook.com>
2023-05-31 19:42:56 +0200

    booking model -class name changed to User

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user, class_name: "User"
-  belongs_to :chef, class_name: "Chef"
+  belongs_to :chef, class_name: "User"
 
   validates :status, presence: true
   validates :date, presence: true
