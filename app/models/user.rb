cbaf57720f506ca5a296d5a0b5b984a6048cac56
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-02 17:06:43 +0200

    fixed mistake in user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, :address, :date_of_birth, presence: true
+  validates :first_name, :last_name, :date_of_birth, :address, presence: true
 
   has_many :reviews
   has_many :bookmarks
