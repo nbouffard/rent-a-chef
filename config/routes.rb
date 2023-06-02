210b201bb8cb725c7e41adf436f20f918edcd445
Quan <99978041+ShimaGunz@users.noreply.github.com>
2023-06-02 01:36:19 +0100

    Update chef profile
    Please check if it looks good

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+
   resources :users, only: %i[index show]
+  resources :users, only: [:index]
+  resources :chefs, only: [:edit, :update]
+
 end
