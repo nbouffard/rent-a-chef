b70b9b9a4b49f716d0679cf55f8fdf0dac78325e
Quan <99978041+ShimaGunz@users.noreply.github.com>
2023-06-03 03:11:09 +0100

    Update Chef profile
    removed separate chef controller.
    merged the chef controller to the user controller.
    
    edited "edit.html.erb" to work with user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,35 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :ensure_chef, only: [:edit, :update]
+
   def index
     @chefs = User.all
   end
 
   def show
     @chef = User.find(params[:id])
+  end
+
+  def edit
+    @chef = current_user
+  end
+
+  def update
+    @chef = current_user
+    if @chef.update(chef_params)
+      redirect_to @chef, notice: 'Profile updated successfully!'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def chef_params
+    params.require(:user).permit(:first_name, :last_name, :bio, :specialties)
+  end
+
+  def ensure_chef
+    redirect_to root_path unless current_user.is_chef?
   end
 end
