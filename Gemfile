770699dbec47b69ef792d70feea722451ab6c09f
Nicola bouffard <84248712+nbouffard@users.noreply.github.com>
2023-06-09 13:00:19 +0200

    add geocoder, update user model

--- Gemfile
+++ Gemfile
@@ -37,6 +37,8 @@ gem "cloudinary"
 
 gem "bootstrap"
 
+gem "geocoder"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", "~> 4.0"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,7 @@ class User < ApplicationRecord
   has_many :reviews
   has_many :bookings
   has_many :messages
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
