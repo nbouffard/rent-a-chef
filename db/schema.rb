063559507b5309bea837271872deeba81e261c68
Lena Sachse <lena.sachse@t-online.de>
2023-06-13 00:29:42 +0200

    -

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_09_105839) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_09_181149) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,9 +104,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_09_105839) do
     t.string "address"
     t.date "date_of_birth"
     t.string "city"
+    t.string "cuisine"
     t.float "latitude"
     t.float "longitude"
-    t.string "cuisine"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
